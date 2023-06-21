9305b892bef0d5c5f6664b05def39e0f5d41e4bb
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-06-21 23:24:46 +0000

    initial commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {
+                'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review
+            }
 
 
 class FileStorage:
@@ -8,9 +21,19 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        new_obj = {}
+
+        if cls is None:
+            return self.__objects
+        else:
+            copy_dic = self.__objects
+            for key in copy_dic:
+                c = key.split('.')
+                if c[0] == cls.__name__:
+                    new_obj[key] = self.__objects[key]
+            return new_obj
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -27,24 +50,18 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __object if it's inside"""
+        if obj is not None:
+            key = str(obj.__class__.__name__) + '.' + str(obj.id)
+            del FileStorage.__objects[key]
+            self.save()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    """Establish a 1-many relationship with the class City and create
+    an attribute called state in class City so the State obj can be
+    accessed from the City objects
+    the cascade tells what happens when State class is modified or deleted
+    In this case, once state is deleted, all City objs are deleted.
+    Once a city obj is dissociated from State, it is deleted
+    """
+    cities = relationship(
+            'City', cascade='all, delete, delete-orphan', backref='state')
+
+    @property
+    def cities(self):
+        """It returns the list of City instances with state_id equals to
+        the current State.id. This is the FileStorage way of creating
+        relationshipbetween State and City.
+        """
+        city_list = []
+        obj_store = models.storage.all("City").values()
+
+        for city in obj_store:
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
