c10d80678d3d850ca834bf198890ab8503fed2a6
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-07-09 17:55:46 +0000

    fix puppet-lint style

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -34,7 +34,7 @@ $nginx_conf = "server {
 	location / {
 		# First attempt to serve request as file, then
 		# as directory, then fall back to displaying a 404.
-		try_files $uri $uri/ =404;
+		try_files ${uri} ${uri}/ =404;
 	}
 	location /hbnb_static/ {
 		alias /data/web_static/current/;
@@ -63,66 +63,66 @@ $nginx_conf = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
+}
 
-file { '/data':
+-> file { '/data':
   ensure  => 'directory'
-} ->
+}
 
-file { '/data/web_static':
+-> file { '/data/web_static':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases':
+-> file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases/test':
+-> file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/shared':
+-> file { '/data/web_static/shared':
 ensure => 'directory'
-  } ->
+  }
 
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "Holberton School Puppet\n"
-} ->
+}
 
-file { '/data/web_static/current':
+-> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
+}
 
-exec { 'chown -R ubuntu:ubuntu /data/':
+-> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html':
+-> file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Holberton School Nginx\n"
-} ->
+}
 
-file { '/var/www/html/404.html':
+-> file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-} ->
+}
 
-file { '/etc/nginx/sites-available/default':
+-> file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-} ->
+}
 
-exec { 'execute ngx':
-  command  => 'sudo service nginx restart',
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+-> exec { 'execute ngx':
+  command => 'sudo service nginx restart',
+  path    => '/usr/bin/:/usr/local/bin/:/bin/'
 }
